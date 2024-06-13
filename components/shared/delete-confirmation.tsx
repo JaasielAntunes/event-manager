68cb733ba4d4d4c01ffe036d38f0d44a7f259758
Jaasiel <69393570+JaasielAntunes@users.noreply.github.com>
2024-06-13 16:08:45 -0300

    add card dos eventos e modal de excluir evento

---
+++ components/shared/delete-confirmation.tsx
@@ -0,0 +1,59 @@
+'use client'
+
+import Image from 'next/image'
+import { usePathname } from 'next/navigation'
+import { useTransition } from 'react'
+
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from '@/components/ui/alert-dialog'
+import { deleteEvent } from '@/lib/actions/event.actions'
+
+export default function DeleteConfirmation({ eventId }: { eventId: string }) {
+  const pathname = usePathname()
+  const [isPending, startTransition] = useTransition()
+
+  return (
+    <AlertDialog>
+      <AlertDialogTrigger>
+        <Image
+          src="/assets/icons/delete.svg"
+          alt="edit"
+          width={20}
+          height={20}
+        />
+      </AlertDialogTrigger>
+
+      <AlertDialogContent className="bg-white">
+        <AlertDialogHeader>
+          <AlertDialogTitle>Tem certeza que deseja excluir?</AlertDialogTitle>
+          <AlertDialogDescription className="p-regular-16 text-grey-600">
+            Esta é uma ação irreversível.
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancelar</AlertDialogCancel>
+
+          <AlertDialogAction
+            onClick={() =>
+              startTransition(async () => {
+                await deleteEvent({ eventId, path: pathname })
+              })
+            }
+          >
+            {isPending ? 'Excluindo...' : 'Excluir'}
+          </AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  )
+}
